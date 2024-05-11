ad8efb38ec151f78d6e92895b7cbbb2fcaf55c3b
Kad-19 <kidusasebe1919@gmail.com>
2024-05-11 20:30:50 +0300

    User Beans

---
+++ Account.java
@@ -0,0 +1,18 @@
+
+public class Account {
+    private String userName;
+    private String password;
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+    public String getUsername(){
+        return userName;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
